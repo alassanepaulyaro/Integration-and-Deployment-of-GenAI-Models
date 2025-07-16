9c77fb8cac893795764aa9156ceb1851c69d7f55
alassane-paul-yaro <alassane-paul.yaro@outlook.fr>
2025-07-16 12:03:21 +0200

    feat: transcription with assemblyai (#5)
    * feat: transcription with assemblyai
    
    * feat: add transcription of local mp3 audio file using AssemblyAI
    
    * feat: deploy Streamlit web application with Docker
    
    ---------
    
    Co-authored-by: Paul Yaro <“alassane-paul.yaro@outlook.fr”>

---
+++ assemblyai_project/main.py
@@ -0,0 +1,13 @@
+import assemblyai as aai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+transcriber = aai.Transcriber()
+
+transcript = transcriber.transcribe("https://assembly.ai/news.mp4")
+# transcript = transcriber.transcribe("./my-local-audio-file.wav")
+
+print(transcript.text)
